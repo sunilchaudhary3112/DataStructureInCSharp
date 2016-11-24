13a23cbd89ebe3f0561736e797280c71ea82a9f8
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2016-11-24 10:45:57 +0530

    Added code few mpre datastructure

---
+++ DataStructureInCSharp/DataStructureInCSharp/Graph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureInCSharp
+{
+    public class AdjMatrixGraph
+    {
+        public bool[,] adjMatrix;
+        public int vertexCount;
+        public AdjMatrixGraph(int vertexCount)
+        {
+            this.vertexCount = vertexCount;
+            adjMatrix = new bool[vertexCount,vertexCount];
+        }
+        public void addEdege(int i, int j)
+        {
+            if(i >=0 && i < vertexCount && j > 0 && j < vertexCount)
+            {
+                adjMatrix[i, j] = true;
+                adjMatrix[j, i] = true;
+            }
+        }
+        public void removeEdge(int i , int j)
+        {
+            if (i >= 0 && i < vertexCount && j > 0 && j < vertexCount)
+            {
+                adjMatrix[i, j] = false;
+                adjMatrix[j, i] = false;
+            }
+        }
+    }
+    public class AdjListGraph
+    {
+        public List<int> Vertices;
+        public LinkedListL[] Edges;
+        public int vertexCount;
+        public AdjListGraph(int vertexCount)
+        {
+            this.vertexCount = vertexCount;
+            Vertices = new List<int>();
+            Edges = new LinkedListL[vertexCount];
+            for (int i = 0; i < vertexCount; i++)
+            {
+                Vertices.Add(i);
+                Edges[i] = new LinkedListL();
+
+            }
+        }
+        public void addeEdge(int source, int dest)
+        {
+            int i = Vertices.IndexOf(source);
+            int j = Vertices.IndexOf(dest);
+            if(i != -1 || j != -1)
+            {
+
+            }
+            
+        }
+            
+
+    }
+}
--- DataStructureInCSharp/DataStructureInCSharp/LinkedList.cs
+++ DataStructureInCSharp/DataStructureInCSharp/LinkedList.cs
@@ -6,28 +6,169 @@ using System.Threading.Tasks;
 
 namespace DataStructureInCSharp
 {
-    class Node
+    public class ListNode
     {
-        int data;
-        Node next;
-        Node(int data)
+        public int data;
+        public ListNode next;
+        public ListNode()
+        {
+            //data = 0;
+            //next = null;
+        }
+        public ListNode(int data)
         {
             this.data = data;
-            next = null;
+        }
+        public void setData(int data)
+        {
+            this.data = data;
+        }
+        public int getdata()
+        {
+            return data;
+        }
+        public void setNext(ListNode next)
+        {
+            this.next = next;
+        }
+        public ListNode getNext()
+        {
+            return this.next;
         }
     }
-    class LinkedList
+    public class LinkedListL
     {
-        Node head;
-
-        public void insert(int data){
-
-        }
-        public void delete(int data)
+        public ListNode head;
+        private int length = 0;
+        public LinkedListL()
         {
+            length = 0;
+        }
+        public ListNode getHead()
+        {
+            return head;
+        }
+        public void insertAtBegin(ListNode node)
+        {
+            node.setNext(head);
+            head = node;
+            length++;
+        }
+        public void insertAtEnd(ListNode node)
+        {
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                ListNode currentNode = head;
+                while (currentNode.getNext() != null)
+                {
+                    currentNode = currentNode.getNext();
+                }
+                currentNode.setNext(node);
+                length++;
+
+
+            }
+        }
+        public void insert(int data, int position)
+        {
+            //fix the length
+            if (position < 0)
+                position = 0;
+            if (position > length)
+                position = length;
+            if (head == null)
+            {
+                head = new ListNode(data);
+            }
+            else if (position == 0)
+            {
+                ListNode newNode = new ListNode(data);
+                newNode.next = head;
+                head = newNode;
+            }
+            else
+            {
+                ListNode cuurentNode = head;
+                for (int i = 1; i < position; i++)
+                {
+                    cuurentNode = cuurentNode.next;
+                }
+                ListNode newNode = new ListNode(data);
+                newNode.next = cuurentNode.next;
+                cuurentNode.setNext(newNode);
+            }
+            length++;
+        }
+        public ListNode removeFromBegin()
+        {
+            ListNode node = head;
+            if (node != null)
+            {
+                head = node.getNext();
+                node.setNext(null);
+            }
+            return node;
+        }
+        public ListNode reomveFromEnd()
+        {
+            ListNode prevNode, currentNode, temp;
+            if (head == null)
+            {
+                return null;
+            }
+            currentNode = head;
+            prevNode = currentNode;
+            if (currentNode.next == null)
+            {
+                head = null;
+                return prevNode;
+            }
+            while (currentNode.next != null)
+            {
+                prevNode = currentNode;
+                currentNode = currentNode.next;
+            }
+            prevNode.next = null;
+            return currentNode;
 
         }
-
+        public void removeMatch(int data)
+        {
+            if (head == null)
+            {
+                return;
+            }
+            if (head.data == data)
+            {
+                head = head.getNext();
+                return;
+            }
+            ListNode cuurentNode = head;
+            ListNode nextNOde = cuurentNode.getNext();
+            while (nextNOde != null)
+            {
+                if (nextNOde.data == data)
+                {
+                    cuurentNode.setNext(nextNOde.getNext());
+                    return;
+                }
+                cuurentNode = nextNOde;
+                nextNOde = nextNOde.next;
+            }
+        }
+        public void display()
+        {
+            ListNode currentNode = head;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+            }
+        }
 
     }
 }
